144ff488ed89ea177513d6e090b8930c21e6f61c
Naur <princessmarielledepalubos2nd@gmail.com>
2026-01-22 17:49:05 +0800

    add player scoring

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const main = document.querySelector("main")
 const win = document.querySelector(".win")
+const left_score = document.querySelector(".left")
+const right_score = document.querySelector(".right")
 const restart_btn = document.querySelector(".restart-btn")
 const symbols = ["A","B","C","D","E","F"]
 let cards_array
@@ -8,6 +10,8 @@ let first_flipped_index
 let first_player = true
 let locked = false
 let won = false
+let score1 = 0
+let score2 = 0
 
 function shuffle() {
     for (let i = 0; i < cards_array.length; i++) {
@@ -20,6 +24,8 @@ function start_game() {
     first_player = true
     won = false
     locked = false
+    score1 = 0
+    score2 = 0
 
     main.innerHTML = ""
 
@@ -65,6 +71,8 @@ function handle_click(index) {
         if (cards_array[first_flipped_index].symbol == cards_array[index].symbol) {
             cards_array[first_flipped_index].matched = true
             cards_array[index].matched = true
+            
+            first_player ? score1 += 1 : score2 += 1
         } else {
             first_player ? first_player = false : first_player = true
         }
@@ -88,6 +96,8 @@ function render() {
     cards_array.forEach((card, index) => {
         (card.flipped || card.matched) ? cards[index].classList.add("flipped") : cards[index].classList.remove("flipped")
     })
+    left_score.innerHTML = score1
+    right_score.innerHTML = score2
     first_player ? main.classList.add("player1") : main.classList.remove("player1")
     won ? win.classList.add("active") : win.classList.remove("active")
 }
